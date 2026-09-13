cb7e89aa6b05e8241bd23f260fc666f1d28bf111
Appolinaire SAWADOGO <sawadogoappolinaire06@gmail.com>
2026-09-13 11:31:09 +0000

    fix: swagger route permitAll

--- backend/auth-service/src/main/java/com/wali/auth_service/security/config/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/wali/auth_service/security/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .sessionManagement(sessionManagement -> sessionManagement.
                         sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/auth/login", "/auth/register").permitAll()
+                        .requestMatchers("/auth/login", "/auth/register", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
--- backend/transaction-service/src/main/java/com/wali/transaction_service/security/config/SecurityConfig.java
+++ backend/transaction-service/src/main/java/com/wali/transaction_service/security/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                 .sessionManagement(sessionManagement -> sessionManagement.
                         sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
                 .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
